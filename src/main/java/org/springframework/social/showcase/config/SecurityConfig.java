96e71a701b652942d2c1e99743da4e17b515e774
John Cao <jhcao23@gmail.com>
2017-07-23 21:07:31 -0400

    no session - stateless

--- src/main/java/org/springframework/social/showcase/config/SecurityConfig.java
+++ src/main/java/org/springframework/social/showcase/config/SecurityConfig.java
@@ -24,6 +24,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 //import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.encrypt.Encryptors;
@@ -69,8 +70,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-//			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-			.formLogin()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+				.formLogin()
 				.loginPage("/login")			//"signin" will cause a crazy error
 				.usernameParameter("username")
 				.passwordParameter("password")
