7b4152e0ef93c40c94c0e554d756d930f9e528d6
John Cao <jhcao23@gmail.com>
2017-08-15 15:36:01 -0700

    $1

--- src/main/java/technology/touchmars/template/service/GenerateUniqueKey.java
+++ src/main/java/technology/touchmars/template/service/GenerateUniqueKey.java
@@ -55,7 +55,16 @@ public class GenerateUniqueKey {
  
 	static private StringBuilder touchmarsEncodeUsingHEX(byte[] touchmarsByte) {
 		StringBuilder touchmarsResult = new StringBuilder();
-		char[] touchmarsKeys = { 'o', 'p', 'q', 'r', 's', 't', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+		char[] touchmarsKeys = 
+			{	  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k'
+				, 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't'
+				, 'u', 'v', 'w', 'x', 'y', 'z'
+				, 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K'
+				, 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T'
+				, 'U', 'V', 'W', 'X', 'Y', 'Z'
+				, '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' 
+				, '+', '-'
+			};
 		for (int index = 0; index < touchmarsByte.length; ++index) {
 			byte myByte = touchmarsByte[index];
  
